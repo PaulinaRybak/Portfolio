18f6fefaf1ee38bf238125b2e2b9688e668663bd
PaulinaR <59685908+PaulinaRybak@users.noreply.github.com>
2020-01-23 15:15:20 +0000

    23.01.20 update menu for wide screen and burger design done

--- java.js
+++ java.js
@@ -1,20 +1,31 @@
+
+
 const navSlide=()=>{
-    const burger = document.querySelector('.burger');
+    const burger = document.querySelector('#burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks= document.querySelectorAll('.nav-links li');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
 
     burger.addEventListener('click',()=>{
+        //toggle nav
         nav.classList.toggle('nav-active');
-
-        navLinks.forEach((link,index)=>{
+        //animate links
+        navLinks.forEach((link, index) => {
             if(link.style.animation){
-                link.style.animation ='';
+                link.style.animation = '';
             } else{
-                link.style.animation =`navLinkFade 0.5s ease forwards ${index/5+0.5}s`;
+                link.style.animation=`navLinkFade 0.5s ease forwards ${index/5 + 0.5}s`;
             }
         });
-        burger.classList.toggle('toggle');
+
+        //burger animation
+
+        burger.classList.toggle("toggle");
+        burger.classList.toggle('open');
+        // nav.classList.toggle('toggle');
+        
     });
+
 }
 
 navSlide();
